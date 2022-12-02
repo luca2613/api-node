013fe2442149d46b101f4b500684938292492e7e
luca2613 <luca.developerpk@gmail.com>
2022-12-02 19:53:11 -0300

    oi2

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.PORT = process.env.PORT || 3000;
 
-exports.DB_HOST = process.env.PORT || "containers-us-west-124.railway.app"
+exports.DB_HOST = process.env.PORT || "localhost"
 exports.DB_USER = process.env.DB_USER || "root"
-exports.DB_PASSWORD = process.env.DB_PASSWORD || "1c6h8fX7tvJMWDhl8gOp"
-exports.DB_NAME = process.env.DB_NAME || "railway"
-exports.DB_PORT = process.env.DB_PORT || 6309
+exports.DB_PASSWORD = process.env.DB_PASSWORD || ""
+exports.DB_NAME = process.env.DB_NAME || "readnow"
+exports.DB_PORT = process.env.DB_PORT || 3306
 
